2a39b22c03ca130d75843d62445035a0cf6af199
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2019-12-16 16:38:13 +0100

    Update deploy-aks-cluster-with-azure-voting-app.sh

--- deploy-aks-cluster-with-azure-voting-app.sh
+++ deploy-aks-cluster-with-azure-voting-app.sh
@@ -14,11 +14,11 @@ VM_RESOURCE_GROUP=$HOSTNAME
 ACR_RESOURCE_GROUP=acr$HOSTNAME
 ACR_NAME=acrname$HOSTNAME
 ACR_LOGIN_SERVER=$ACR_NAME.azurecr.io
-AKS_RESOURCE_GROUP=akscluster
+AKS_CLUSTER_NAME=akscluster
 #validate the variables
 echo -e "Variables"
 echo ""
-echo -e "\nHOSTNAME="$HOSTNAME"\nUSER="$USER"\nAZ_ACCOUNT="$AZ_ACCOUNT"\nVM_RESOURCE_GROUP="$VM_RESOURCE_GROUP"\nACR_RESOURCE_GROUP="$ACR_RESOURCE_GROUP"\nAKS_RESOURCE_GROUP="MC_$ACR_RESOURCE_GROUP"_"$AKS_RESOURCE_GROUP"_westeurope\n"
+echo -e "\nHOSTNAME="$HOSTNAME"\nUSER="$USER"\nAZ_ACCOUNT="$AZ_ACCOUNT"\nVM_RESOURCE_GROUP="$VM_RESOURCE_GROUP"\nACR_RESOURCE_GROUP="$ACR_RESOURCE_GROUP"\nAKS_CLUSTER_NAME="$AKS_CLUSTER_NAME"\nAKS_RESOURCE_GROUP="MC_$ACR_RESOURCE_GROUP"_"$AKS_CLUSTER_NAME"_westeurope\n"
 echo "continue (Y/N)"
 read Response
 if [ $Response = "N" ] || [ $Response = "n" ]
@@ -51,11 +51,11 @@ sleep 120
 echo -e "\n##### 11 - Update the name of the Registry <acrName> with this of your instance <arcId>#####\n"
 az role assignment create --assignee $APPID --scope   "$ACRID" --role acrpull
 echo -e "\n##### 12 - Create the Kubernetes cluster#####\n"
-az aks create --resource-group $ACR_RESOURCE_GROUP --name $AKS_RESOURCE_GROUP  --node-count 1 --service-principal "$APPID" --client-secret "$PASSWORD" --generate-ssh-keys
+az aks create --resource-group $ACR_RESOURCE_GROUP --name $AKS_CLUSTER_NAME  --node-count 1 --service-principal "$APPID" --client-secret "$PASSWORD" --generate-ssh-keys
 echo -e "\n##### 13 - Install Kubectl #####\n"
 sudo az aks install-cli
 echo -e "\n##### 14 - Log to the cluster with kubectl#####\n"
-az aks get-credentials --resource-group $ACR_RESOURCE_GROUP --name $AKS_RESOURCE_GROUP
+az aks get-credentials --resource-group $ACR_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 echo -e "\n##### 15 - verify the connection to your cluster with kubectl#####\n"
 kubectl get nodes
 echo -e "\n##### 16 - update the manifeste file#####\n"
