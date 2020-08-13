b959fcbec88511341a8248a5ace5ca8f579043c3
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2020-08-13 19:36:50 +0200

    Update deploy-aks-cluster-voting-app-with-service-principal.sh

--- deploy-aks-cluster-voting-app-with-service-principal.sh
+++ deploy-aks-cluster-voting-app-with-service-principal.sh
@@ -99,39 +99,28 @@ chmod +x $DELETE_FILE
 echo "echo "$ACR_RESOURCE_GROUP >> $DELETE_FILE
 echo "az group delete --name "$ACR_RESOURCE_GROUP" --y" >> $DELETE_FILE
 echo -e "\n##### 6 - Create an Azure Container Registry ACR and log in to the container registry#####\n"
-az acr create --resource-group $ACR_RESOURCE_GROUP --name $ACR_NAME --sku Basic 
+az acr create --resource-group $ACR_RESOURCE_GROUP --name $ACR_NAME --sku Free 
 az acr login --name $ACR_NAME
 echo -e "\n##### 7 - Tag your image azure-vote-front with the acrLoginServer and "$USER"#####\n"
 docker tag azure-vote-front  $ACR_LOGIN_SERVER/azure-vote-front:$USER
 echo -e "\n##### 8 - Push  the docker image in registry ACR #####\n"
 docker push $ACR_LOGIN_SERVER/azure-vote-front:$USER
-echo -e "\n##### 9 - Create a service principal #####\n"
-#SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --skip-assignment)
-#echo -e "\n##### 10 - Get the APPID, the PASSWORD and the ACRID #####\n"
-#APPID=$(echo "$SERVICE_PRINCIPAL"|grep appId|cut -d '"' -f 4)
-#PASSWORD=$(echo "$SERVICE_PRINCIPAL"|grep password|cut -d '"' -f 4)
-ACRID=$(az acr show --resource-group $ACR_RESOURCE_GROUP --name $ACR_NAME --query "id" --output tsv)
-#echo "$PASSWORD"
-#wait 2 minutes to be sure the resource exist
-#sleep 120
-#echo -e "\n##### 11 - Update the name of the Registry <acrName> with this of your instance <arcId>#####\n"
-#az role assignment create --assignee $APPID --scope   "$ACRID" --role acrpull
-echo -e "\n##### 12 - Create the Kubernetes cluster#####\n"
+echo -e "\n##### 9 - Create the Kubernetes cluster#####\n"
 az aks create --resource-group $ACR_RESOURCE_GROUP --name $AKS_CLUSTER_NAME  --node-count 1 --service-principal "$APPID" --client-secret "$PASSWORD" --generate-ssh-keys
 ##create the script shell to delete the resource groups of this lab
 echo "echo MC_"$ACR_RESOURCE_GROUP"_"$AKS_CLUSTER_NAME"_"$REGION >> $DELETE_FILE
 echo "az group delete --name MC_"$ACR_RESOURCE_GROUP"_"$AKS_CLUSTER_NAME"_"$REGION" --y" >> $DELETE_FILE
-echo -e "\n##### 13 - Install Kubectl #####\n"
+echo -e "\n##### 10 - Install Kubectl #####\n"
 sudo az aks install-cli
-echo -e "\n##### 14 - Log to the cluster with kubectl#####\n"
+echo -e "\n##### 11 - Log to the cluster with kubectl#####\n"
 az aks get-credentials --resource-group $ACR_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
-echo -e "\n##### 15 - verify the connection to your cluster with kubectl#####\n"
+echo -e "\n##### 12 - verify the connection to your cluster with kubectl#####\n"
 kubectl get nodes
-echo -e "\n##### 16 - update the manifeste file#####\n"
+echo -e "\n##### 13 - update the manifeste file#####\n"
 sed -i "s/microsoft\/azure-vote-front:v1/$ACR_LOGIN_SERVER\/azure-vote-front:$USER/" ./azure-vote-all-in-one-redis.yaml
-echo -e "\n##### 17 - Deploy the application #####\n"
+echo -e "\n##### 14 - Deploy the application #####\n"
 kubectl apply -f azure-vote-all-in-one-redis.yaml
-echo -e "\n##### 18 - Wait until the application starts #####\n"
+echo -e "\n##### 15 - Wait until the application starts #####\n"
 IPCLUSTER=$(kubectl get service azure-vote-front| grep azure | cut -d" " -f 10)
 echo "EXTERNAL IP : "$IPCLUSTER
 while [ $IPCLUSTER = "<pending>" ]
